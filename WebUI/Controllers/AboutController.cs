bad9c8f4b94d80a0f790d1ed3222fb56824c5ae8
Yusuf İşleyen <me@yusufisleyen.com>
2021-10-15 00:18:09 +0300

    Hakkımzıda sayfası eklendi

---
+++ WebUI/Controllers/AboutController.cs
@@ -0,0 +1,26 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Controllers
+{
+    public class AboutController : Controller
+    {
+        AboutManager aboutManager = new AboutManager(new EfAboutRepository());
+
+        public IActionResult Index()
+        {
+            var about = aboutManager.GetAll();
+            return View(about);
+        }
+
+        public PartialViewResult SocialMedia()
+        {
+            return PartialView();
+        }
+    }
+}
