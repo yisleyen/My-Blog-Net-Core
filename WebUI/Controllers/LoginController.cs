0fec8f3afe5aa0680e4f8c9e5f8455f987c1974f
Yusuf İşleyen <me@yusufisleyen.com>
2021-10-16 15:44:08 +0300

    Login işlemi yapıldı

--- WebUI/Controllers/LoginController.cs
+++ WebUI/Controllers/LoginController.cs
@@ -1,16 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccess.Concrete;
+using Entity.Concrete;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace WebUI.Controllers
 {
+    [AllowAnonymous]
     public class LoginController : Controller
     {
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(Writer writer)
+        {
+            Context context = new Context();
+
+            var loginValue = context.Writers.FirstOrDefault(x => x.Email == writer.Email && x.Password == writer.Password);
+
+            if (loginValue != null) // login işlemi başarılı
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Email, writer.Email)
+                };
+
+                var userIdentity = new ClaimsIdentity(claims, "a");
+
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(userIdentity);
+
+                await HttpContext.SignInAsync(claimsPrincipal);
+
+                return RedirectToAction("Index", "Writer");
+            }
+            else // login işlemi başarısız
+            {
+                return View();
+            }
+
         }
     }
 }
--- WebUI/Startup.cs
+++ WebUI/Startup.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +27,17 @@ namespace WebUI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddMvc(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build();
+                config.Filters.Add(new AuthorizeFilter(policy));
+            });
+
+            services.AddMvc();
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(x => x.LoginPath = "/Login/Index");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +58,8 @@ namespace WebUI
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+
+            app.UseAuthentication();
 
             app.UseRouting();
 
