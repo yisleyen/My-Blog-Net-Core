87fec20a9d1ba91c75f0ea97961ecfd7ca5310a8
Yusuf İşleyen <me@yusufisleyen.com>
2021-10-16 14:50:01 +0300

    Arayüz bölümnde link düzenlemeleri yapıldı

---
+++ WebUI/Controllers/ContactController.cs
@@ -0,0 +1,31 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Entity.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Controllers
+{
+    public class ContactController : Controller
+    {
+        ContactManager contactManager = new ContactManager(new EfContactRepository());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Contact contact)
+        {
+            contact.Status = true;
+            contact.CreatedDate = DateTime.Now;
+            contactManager.Insert(contact);
+            return RedirectToAction("Index", "Blog");
+        }
+    }
+}
