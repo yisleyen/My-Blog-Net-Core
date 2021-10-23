d241817746c1fc3cc0bf362451676785a6ae7c0c
Yusuf İşleyen <me@yusufisleyen.com>
2021-10-24 02:36:35 +0300

    Yazar paneli blog ekleme hazırlandı

---
+++ Business/ValidationRules/BlogValidator.cs
@@ -0,0 +1,27 @@
+﻿using Entity.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules
+{
+    public class BlogValidator : AbstractValidator<Blog>
+    {
+        public BlogValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Başlık boş geçilemez");
+            RuleFor(x => x.Content).NotEmpty().WithMessage("İçerik boş geçilemez");
+            RuleFor(x => x.Image).NotEmpty().WithMessage("Görsel boş geçilemez");
+            RuleFor(x => x.ThumbnailImage).NotEmpty().WithMessage("Küçük görsel boş geçilemez");
+            RuleFor(x => x.CategoryId).NotEmpty().WithMessage("Kategori boş geçilemez");
+
+            RuleFor(x => x.Title).MinimumLength(10).WithMessage("Başlık minimum 10 karakter olmalıdır.");
+            RuleFor(x => x.Title).MaximumLength(150).WithMessage("Başlık maksimum 150 karakter olmalıdır.");
+
+            RuleFor(x => x.Content).MinimumLength(100).WithMessage("İçerik minimum 100 karakter olmalıdır.");
+        }
+    }
+}
--- WebUI/Controllers/BlogController.cs
+++ WebUI/Controllers/BlogController.cs
@@ -1,7 +1,11 @@
 ﻿using Business.Concrete;
+using Business.ValidationRules;
 using DataAccess.EntityFramework;
+using Entity.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -39,6 +43,49 @@ namespace WebUI.Controllers
             var blogList = blogManager.GetAllByWriter(4);
 
             return View(blogList);
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Add()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
+            List<SelectListItem> categoryList = (from x in categoryManager.GetAll()
+                                                 select new SelectListItem
+                                                 {
+                                                     Text = x.Name,
+                                                     Value = x.Id.ToString()
+                                                 }).ToList();
+            ViewBag.CategorySelectList = categoryList;
+
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Add(Blog blog)
+        {
+            BlogValidator blogValidator = new BlogValidator();
+            ValidationResult validationResult = blogValidator.Validate(blog);
+
+            if (validationResult.IsValid)
+            {
+                blog.WriterId = 4;
+                blog.Status = true;
+                blog.CreatedDate = DateTime.Now;
+                blogManager.Insert(blog);
+
+                return RedirectToAction("WriterBlogList", "Blog");
+            }
+            else
+            {
+                foreach (var item in validationResult.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+
+            return View();
         }
     }
 }
