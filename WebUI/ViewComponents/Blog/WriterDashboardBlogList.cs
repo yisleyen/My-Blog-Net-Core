edaa447351ec2bcc0122087af7a7484143d27468
Yusuf İşleyen <me@yusufisleyen.com>
2021-10-29 02:14:42 +0300

    Yazar paneli dashboard düzenlemelerine devam edildi

---
+++ WebUI/ViewComponents/Blog/WriterDashboardBlogList.cs
@@ -0,0 +1,22 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.ViewComponents.Blog
+{
+    public class WriterDashboardBlogList : ViewComponent
+    {
+        BlogManager blogManager = new BlogManager(new EfBlogRepository());
+
+        public IViewComponentResult Invoke(int id)
+        {
+            var blogList = blogManager.GetAllWithCategory();
+
+            return View(blogList);
+        }
+    }
+}
