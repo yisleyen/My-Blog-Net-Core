ca01d6ed8be9bf5683741f387a98b9d2f20a87b8
Yusuf İşleyen <me@yusufisleyen.com>
2021-10-29 15:23:22 +0300

    Yazar paneli dashboard düzenlemelerine devam edildi

---
+++ WebUI/ViewComponents/Category/WriterDashboardCategoryList.cs
@@ -0,0 +1,22 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.ViewComponents.Category
+{
+    public class WriterDashboardCategoryList : ViewComponent
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var categoryList = categoryManager.GetAll();
+
+            return View(categoryList);
+        }
+    }
+}
