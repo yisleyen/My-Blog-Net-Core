877d3ad40a263f007ade4b9ada85b1c1cae73a87
Yusuf İşleyen <me@yusufisleyen.com>
2021-11-03 23:25:24 +0300

    Yazar paneli dashboard yazar bilgisi eklendi

---
+++ WebUI/ViewComponents/Writer/WriterInfoOnDashboard.cs
@@ -0,0 +1,22 @@
+﻿using Business.Abstract;
+using DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.ViewComponents.Writer
+{
+    public class WriterInfoOnDashboard : ViewComponent
+    {
+        WriterManager writerManager = new WriterManager(new EfWriterRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var writer = writerManager.GetById(4);
+
+            return View(writer);
+        }
+    }
+}
