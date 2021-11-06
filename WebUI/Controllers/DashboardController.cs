c52a49ec1f3e807e2015b65ca858da2dd3836280
Yusuf İşleyen <me@yusufisleyen.com>
2021-11-06 20:46:25 +0300

    Yazar paneli istatistikler eklendi

--- WebUI/Controllers/DashboardController.cs
+++ WebUI/Controllers/DashboardController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,9 +11,16 @@ namespace WebUI.Controllers
 {
     public class DashboardController : Controller
     {
+        BlogManager blogManager = new BlogManager(new EfBlogRepository());
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
+
         [AllowAnonymous]
         public IActionResult Index()
         {
+            ViewBag.TotalBlogCount = blogManager.GetAll().Count();
+            ViewBag.WriterBlogCount = blogManager.GetAllByWriter(4).Count();
+            ViewBag.CategoryCount = categoryManager.GetAll().Count();
+
             return View();
         }
     }
