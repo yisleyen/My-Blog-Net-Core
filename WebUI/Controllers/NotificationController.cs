814edc594ba932db2953d76d266b92f028c11b7a
Yusuf İşleyen <me@yusufisleyen.com>
2021-11-17 19:35:04 +0300

    Yazar paneli bildirimler sayfası hazırlandı

--- WebUI/Controllers/NotificationController.cs
+++ WebUI/Controllers/NotificationController.cs
@@ -1,16 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebUI.Controllers
 {
     public class NotificationController : Controller
     {
+        NotificationManager notificationManager = new NotificationManager(new EfNotificationRepository());
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult List()
+        {
+            var notifications = notificationManager.GetAll();
+
+            return View(notifications);
         }
     }
 }
