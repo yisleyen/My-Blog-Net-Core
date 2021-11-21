32e6c145b387d1b0e3c44897f9bfe895ce809dcf
Yusuf İşleyen <me@yusufisleyen.com>
2021-11-21 14:20:17 +0300

    Yazar paneli mesaj listesi ve detay sayfası hazırlandı

---
+++ WebUI/Controllers/MessageController.cs
@@ -0,0 +1,31 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Controllers
+{
+    [AllowAnonymous]
+    public class MessageController : Controller
+    {
+        Message2Manager messageManager = new Message2Manager(new EfMessage2Repository());
+
+        public IActionResult List()
+        {
+            var messages = messageManager.GetInboxListByWriter(4);
+
+            return View(messages);
+        }
+
+        public IActionResult MessageDetails(int id)
+        {
+            var messaga = messageManager.GetById(id);
+
+            return View(messaga);
+        }
+    }
+}
