25daabfd1415598d378401d87cccf4022b25fea9
yisleyen <isleyenyusuf@gmail.com>
2021-11-26 14:37:14 +0300

    Admin paneli category controller eklendi

---
+++ WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- WebUI/Startup.cs
+++ WebUI/Startup.cs
@@ -67,9 +67,14 @@ namespace WebUI
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
                 endpoints.MapControllerRoute(name: "blog",
                                 pattern: "blog/{title}-{id}",
                                 defaults: new { controller = "Blog", action = "Details" });
+
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
