bcf8c5eaf790e3c1e21ab559ecdecb543599587b
Yusuf İşleyen <me@yusufisleyen.com>
2021-12-07 00:05:24 +0300

    Admin paneli blog listesi excel export eklendi

--- WebUI/Areas/Admin/Controllers/BlogController.cs
+++ WebUI/Areas/Admin/Controllers/BlogController.cs
@@ -1,8 +1,10 @@
 ﻿using Business.Concrete;
+using ClosedXML.Excel;
 using DataAccess.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using X.PagedList;
@@ -19,6 +21,32 @@ namespace WebUI.Areas.Admin.Controllers
             var blogs = blogManager.GetAll().ToPagedList(page, 12);
 
             return View(blogs);
+        }
+
+        public IActionResult ExportExcelFile()
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Bloglar");
+                worksheet.Cell(1, 1).Value = "ID";
+                worksheet.Cell(1, 2).Value = "Title";
+
+                int RowCount = 2;
+
+                foreach (var item in blogManager.GetAll())
+                {
+                    worksheet.Cell(RowCount, 1).Value = item.Id;
+                    worksheet.Cell(RowCount, 2).Value = item.Title;
+                    RowCount++;
+                }
+
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Blogs.xlsx");
+                }
+            }
         }
     }
 }
