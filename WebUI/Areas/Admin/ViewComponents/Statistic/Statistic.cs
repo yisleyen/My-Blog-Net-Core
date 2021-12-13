90e22312716b03d7309a74e48ee615007cf2f20a
Yusuf İşleyen <me@yusufisleyen.com>
2021-12-13 22:12:39 +0300

    Admin paneli istatistikler sayfası düzenleme v1

---
+++ WebUI/Areas/Admin/ViewComponents/Statistic/Statistic.cs
@@ -0,0 +1,23 @@
+﻿using DataAccess.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic : ViewComponent
+    {
+        Context context = new Context();
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.BlogCount = context.Blogs.Count();
+            ViewBag.CommentCount = context.Comments.Count();
+            ViewBag.ContactCount = context.Contacts.Count();
+
+            return View();
+        }
+    }
+}
