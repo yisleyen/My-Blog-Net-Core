eee8d197d106933c10ded255ff02ea6dcc539d5d
Yusuf İşleyen <me@yusufisleyen.com>
2021-12-15 22:25:50 +0300

    Admin paneli admin bilgileri istatistikler bölümüne eklendi

--- WebUI/Areas/Admin/ViewComponents/StatisticAdmin/StatisticAdmin.cs
+++ WebUI/Areas/Admin/ViewComponents/StatisticAdmin/StatisticAdmin.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using DataAccess.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebUI.Areas.Admin.ViewComponents.StatisticAdmin
 {
-    public class StatisticAdmin
+    public class StatisticAdmin : ViewComponent
     {
+        Context context = new Context();
 
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.AdminName = context.Admins.Where(x => x.Id == 1).Select(y => y.Name).FirstOrDefault();
+            ViewBag.AdminImage = context.Admins.Where(x => x.Id == 1).Select(y => y.ImageUrl).FirstOrDefault();
+            ViewBag.AdminDesc = context.Admins.Where(x => x.Id == 1).Select(y => y.ShortDescription).FirstOrDefault();
+
+            return View();
+        }
     }
 }
