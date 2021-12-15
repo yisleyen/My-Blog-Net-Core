4f6dec1b0abd128545874b8fde362464dc44978c
Yusuf İşleyen <me@yusufisleyen.com>
2021-12-15 22:00:22 +0300

    Admin paneli için service, manager, repository eklendi

---
+++ Business/Concrete/AdminManager.cs
@@ -0,0 +1,46 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class AdminManager : IAdminService
+    {
+        IAdminDal _adminDal;
+
+        public AdminManager(IAdminDal adminDal)
+        {
+            _adminDal = adminDal;
+        }
+
+        public void Delete(Admin entity)
+        {
+            _adminDal.Delete(entity);
+        }
+
+        public List<Admin> GetAll()
+        {
+            return _adminDal.GetAll();
+        }
+
+        public Admin GetById(int id)
+        {
+            return _adminDal.GetById(id);
+        }
+
+        public void Insert(Admin entity)
+        {
+            _adminDal.Insert(entity);
+        }
+
+        public void Update(Admin entity)
+        {
+            _adminDal.Update(entity);
+        }
+    }
+}
