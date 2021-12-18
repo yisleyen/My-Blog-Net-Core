51776b7e06c901538b151a6aaf1942a43726d96b
Yusuf İşleyen <me@yusufisleyen.com>
2021-12-19 00:26:05 +0300

    Admin paneli hava durumu bölümü güncellendi

---
+++ DataAccess/Migrations/20211218210737_mig_admintable_weather_and_city_coloum_add.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class mig_admintable_weather_and_city_coloum_add : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Admins",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "WeatherApiKey",
+                table: "Admins",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Admins");
+
+            migrationBuilder.DropColumn(
+                name: "WeatherApiKey",
+                table: "Admins");
+        }
+    }
+}
