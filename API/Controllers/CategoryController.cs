80413cb6326dbb206c35d8d7dded76aa13d81668
Yusuf İşleyen <me@yusufisleyen.com>
2022-01-18 22:05:04 +0300

    Api projesi eklendi

---
+++ API/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
+
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var categoryList = categoryManager.GetAll();
+
+            return Ok(categoryList);
+        }
+    }
+}
