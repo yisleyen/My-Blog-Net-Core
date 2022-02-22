91a89548bed7aefaa8a3f75572554935b4b521fc
Yusuf İşleyen <me@yusufisleyen.com>
2022-02-22 23:20:54 +0300

    Identity kütüphanesi ile kayıt işlemine başlandı

---
+++ WebUI/Controllers/SignUpController.cs
@@ -0,0 +1,56 @@
+﻿using Entity.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebUI.Models;
+
+namespace WebUI.Controllers
+{
+    [AllowAnonymous]
+    public class SignUpController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public SignUpController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserSignUpViewModel userSignUpViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser()
+                {
+                    Email = userSignUpViewModel.Email,
+                    UserName = userSignUpViewModel.UserName,
+                    NormalizedUserName = userSignUpViewModel.NormalizedUserName
+                };
+
+                var result = await _userManager.CreateAsync(appUser, userSignUpViewModel.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Login");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+
+            return View(userSignUpViewModel);
+        }
+    }
+}
---
+++ WebUI/Models/UserSignUpViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebUI.Models
+{
+    public class UserSignUpViewModel
+    {
+        [Display(Name = "Kullanıcı Adı")]
+        [Required(ErrorMessage = "Kullanıcı adı giriniz")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Ad - Soyad")]
+        [Required(ErrorMessage = "Ad - Soyad giriniz")]
+        public string NormalizedUserName { get; set; }
+
+        [Display(Name = "Şifre")]
+        [Required(ErrorMessage = "Şifre giriniz")]
+        public string Password { get; set; }
+
+        [Display(Name = "Şifre Tekrar")]
+        [Compare("Password",ErrorMessage ="Girmiş olduğunuz şifreler aynı olmalıdır")]
+        public string ConfirmPassword { get; set; }
+
+        [Display(Name = "E-Posta Adresi")]
+        [Required(ErrorMessage = "E-posta adresi giriniz")]
+        public string Email { get; set; }
+    }
+}
