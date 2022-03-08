7b3639b6ec95c80bfe37c94b11e0f1cb6a34c8c7
Yusuf İşleyen <me@yusufisleyen.com>
2022-03-08 17:49:34 +0300

    Identity kütüphanesi ile giriş işlemi

--- WebUI/Controllers/LoginController.cs
+++ WebUI/Controllers/LoginController.cs
@@ -3,50 +3,75 @@ using Entity.Concrete;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using WebUI.Models;
 
 namespace WebUI.Controllers
 {
     [AllowAnonymous]
     public class LoginController : Controller
     {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public LoginController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
         [HttpPost]
-        public async Task<IActionResult> Index(Writer writer)
+        public async Task<IActionResult> Index(UserSignInViewModel userSignInViewModel)
         {
-            Context context = new Context();
-
-            var loginValue = context.Writers.FirstOrDefault(x => x.Email == writer.Email && x.Password == writer.Password);
-
-            if (loginValue != null) // login işlemi başarılı
+            if (ModelState.IsValid)
             {
-                var claims = new List<Claim>
+                var result = await _signInManager.PasswordSignInAsync(userSignInViewModel.UserName, userSignInViewModel.Password, false, true);
+
+                if (result.Succeeded)
                 {
-                    new Claim(ClaimTypes.Name, writer.Email)
-                };
-
-                var userIdentity = new ClaimsIdentity(claims, "a");
-
-                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(userIdentity);
-
-                await HttpContext.SignInAsync(claimsPrincipal);
-
-                return RedirectToAction("Index", "Dashboard");
-            }
-            else // login işlemi başarısız
-            {
-                return View();
+                    return RedirectToAction("Index", "Dashboard");
+                }
             }
 
+            return View();
         }
+
+        //[HttpPost]
+        //public async Task<IActionResult> Index(Writer writer)
+        //{
+        //    Context context = new Context();
+
+        //    var loginValue = context.Writers.FirstOrDefault(x => x.Email == writer.Email && x.Password == writer.Password);
+
+        //    if (loginValue != null) // login işlemi başarılı
+        //    {
+        //        var claims = new List<Claim>
+        //        {
+        //            new Claim(ClaimTypes.Name, writer.Email)
+        //        };
+
+        //        var userIdentity = new ClaimsIdentity(claims, "a");
+
+        //        ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(userIdentity);
+
+        //        await HttpContext.SignInAsync(claimsPrincipal);
+
+        //        return RedirectToAction("Index", "Dashboard");
+        //    }
+        //    else // login işlemi başarısız
+        //    {
+        //        return View();
+        //    }
+
+        //}
     }
 }
---
+++ WebUI/Models/UserSignInViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebUI.Models
+{
+    public class UserSignInViewModel
+    {
+        [Display(Name = "Kullanıcı Adı")]
+        [Required(ErrorMessage = "Kullanıcı adı giriniz")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Şifre")]
+        [Required(ErrorMessage = "Şifre giriniz")]
+        public string Password { get; set; }
+    }
+}
