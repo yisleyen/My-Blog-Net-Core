347183b3d27f9468a4231377ce7122fd79693241
Yusuf İşleyen <me@yusufisleyen.com>
2022-03-10 23:30:29 +0300

    Mail bülteni abone olma ajax çevrildi

--- WebUI/Controllers/NewsletterController.cs
+++ WebUI/Controllers/NewsletterController.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Concrete;
 using DataAccess.EntityFramework;
 using Entity.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace WebUI.Controllers
 {
+    [AllowAnonymous]
     public class NewsletterController : Controller
     {
         NewsletterManager newsletterManager = new NewsletterManager(new EfNewsletterRepository());
@@ -20,7 +22,7 @@ namespace WebUI.Controllers
         }
 
         [HttpPost]
-        public PartialViewResult Subscribe(Newsletter newsletter)
+        public IActionResult Subscribe(Newsletter newsletter)
         {
             newsletter.Status = true;
             newsletter.CreatedDate = DateTime.Now;
