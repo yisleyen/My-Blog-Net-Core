1f8160f473f69db9de1d566bf2beffcb1403b92c
Yusuf İşleyen <me@yusufisleyen.com>
2022-03-26 00:08:54 +0300

    Admin paneli yorum listeleme tamamlandı

---
+++ WebUI/Areas/Admin/Controllers/CommentController.cs
@@ -0,0 +1,24 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CommentController : Controller
+    {
+        CommentManager commentManager = new CommentManager(new EfCommetRepository());
+
+        public IActionResult Index()
+        {
+            var comments = commentManager.GetAll();
+
+            return View(comments);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+    }
+}
