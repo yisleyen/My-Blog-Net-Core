64e9ec81114e96b13fa2f06d07b8284d4ce12a38
Yusuf İşleyen <me@yusufisleyen.com>
2022-03-27 00:38:44 +0300

    Admin paneli rol listesi, rol ekleme ve silme işlemi yapıldı

---
+++ WebUI/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,75 @@
+﻿using Entity.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using WebUI.Models;
+
+namespace WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public RoleController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var roles = _roleManager.Roles.ToList();
+
+            return View(roles);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(RoleViewModel roleViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                AppRole appRole = new AppRole
+                {
+                    Name = roleViewModel.Name
+                };
+
+                var result = await _roleManager.CreateAsync(appRole);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+
+            return View(roleViewModel);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var roles = _roleManager.Roles.FirstOrDefault(x => x.Id == id);
+
+            var result = await _roleManager.DeleteAsync(roles);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ WebUI/Models/RoleViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace WebUI.Models
+{
+    public class RoleViewModel
+    {
+        public string Name { get; set; }
+    }
+}
